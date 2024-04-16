6f6a5d1299848446b332d884979adb18c0d0cc74
Roan Richmond <roan.richmond@codethink.co.uk>
2024-04-16 09:26:14 +0100

    magic-rust: Use libs in main function Cargo.toml: Added libs to workspace
    Signed-off-by: Roan Richmond <roan.richmond@codethink.co.uk>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use mtg_cards;
+
+#[tokio::main]
+async fn main() {
+    let card = mtg_cards::id_find(386616).await;
+    match card {
+        Ok(c) => println!("All is good!\n{:?}", c),
+        Err(e) => println!("All is not good?\n{:?}", e),
+    }
 }
